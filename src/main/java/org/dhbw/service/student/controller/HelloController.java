2881e525cb94eddd423502fbc72ad4442505abff
Patrick Lorbach <patrick_lorbach@outlook.de>
2020-05-04 10:47:41 +0200

    Initial Commit

---
+++ src/main/java/org/dhbw/service/student/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.dhbw.service.student.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+* HelloController
+*/
+@RestController
+public class HelloController {
+
+ @GetMapping("/hello")
+ public String sayHello() {
+ return "Hallo Welt!";
+ }
+
+}
